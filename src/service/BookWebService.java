59be93efa9662e106223fb05029c1f219a3b72dd
jhszcsb <jhszcsb@gmail.com>
2015-05-20 23:19:29 +0200

    deployment and WSDL working for Book entity

--- src/service/BookWebService.java
+++ src/service/BookWebService.java
@@ -3,6 +3,7 @@ package service;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
@@ -11,37 +12,34 @@ import javax.jws.WebService;
 import dal.BookFacade;
 import entity.Book;
 
-
 @Stateless
 @WebService
 public class BookWebService {
 	
-	private List<BookModel> books = null;
-	//private List<Book> books = null;
-	
-	//@EJB
-	//private BookFacade facade;
-	
-	//private BookService service;
+	private List<BookModel> books = null;	// for dummy service
+	private List<Book> books2 = null;
 	
 	public BookWebService() {
-		books = new ArrayList<BookModel>();
+		books = new ArrayList<BookModel>();	// for dummy service
 		books.add(new BookModel("Asdf"));
-		
-		//service = new BookService();
-		//books = service.findAllBooks();
-		//books = null;
-		//books = facade.findAll();
+	}
+	
+	@PostConstruct
+	public void readBooksFromDB() {
+		BookFacade facade = new BookFacade();
+		List<Book> booksList = new ArrayList<Book>();
+		booksList = facade.findAll();
+		books2 = booksList;
 	}
 	
 	@WebMethod
-	public List<BookModel> getBooks() {
+	public List<BookModel> getBooks() {		// for dummy service
 		return books;
 	}
 	
-	/*@WebMethod
-	public List<Book> getBooks() {
-		return books;
-	}*/
+	@WebMethod
+	public List<Book> getBooks2() {
+		return books2;
+	}
 
 }
